8f6faf879b4c98ed26b6e26073342ccf0d396da7
bas <bas@ya.ru>
2019-07-31 17:35:10 +0300

    Остался XS

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,13 @@
-//import $ from "jquery";
-//import Swiper from 'swiper';
+import $ from "jquery";
 
-//import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
-//Swiper.use([Navigation, Pagination]);
+import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
+Swiper.use([Navigation, Pagination]);
 
 //window.jQuery = $;
 //window.$ = $;
 //require('@fancyapps/fancybox');
 
-//$(function() {
+$(function() {
 
     /* - - - Подключение fancybox - - - */
 /*     $('[data-fancybox]').fancybox({
@@ -22,4 +21,15 @@
             "close"
         ],
     }); */
-//});
+
+    var block_name = 'block-result';
+
+    var sliderResult = new Swiper($('.' + block_name + '__slider .swiper-container'), {
+        slideActiveClass: block_name + '__slide-active',
+        navigation: {
+            prevEl: $('.' + block_name + '__prev'),
+            nextEl: $('.' + block_name + '__next'),
+            disabledClass: block_name + '__str-disabled',
+        },
+    });
+});
