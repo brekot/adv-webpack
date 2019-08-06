f1f813ec77bc79d2cc3c24e2b6c197c953f7bc7d
bas <bas@ya.ru>
2019-08-06 09:56:11 +0300

    Прокрутка к элементам

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,14 @@ import $ from "jquery";
 import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
 Swiper.use([Navigation, Pagination]);
 
-//window.jQuery = $;
-//window.$ = $;
-//require('@fancyapps/fancybox');
+window.jQuery = $;
+window.$ = $;
+require('@fancyapps/fancybox');
 
 $(function() {
 
-    /* - - - Подключение fancybox - - - */
-/*     $('[data-fancybox]').fancybox({
+    /* Подключение fancybox */
+    $('[data-fancybox]').fancybox({
         buttons: [
             "zoom",
             //"share",
@@ -20,19 +20,35 @@ $(function() {
             //"thumbs",
             "close"
         ],
-    }); */
+    });
 
-    var block_name = 'block-result';
+    /* - - - Прокрутка к элементу - - - */
+    $(".go-to").click(function() {
 
-    var sliderResult = new Swiper($('.' + block_name + '__slider .swiper-container'), {
-        slideActiveClass: block_name + '__slide-active',
+        var elem = $(this).attr('href');
+
+        $('html, body').animate({
+
+            scrollTop: $(elem).offset().top
+
+        }, 1000);
+
+        return false;
+    });
+
+    /* Слайдер */
+    var strSliderResult = 'block-result';
+
+    var sliderResult = new Swiper($('.' + strSliderResult + '__slider .swiper-container'), {
+        slideActiveClass: strSliderResult + '__slide-active',
         navigation: {
-            prevEl: $('.' + block_name + '__prev'),
-            nextEl: $('.' + block_name + '__next'),
-            disabledClass: block_name + '__str-disabled',
+            prevEl: $('.' + strSliderResult + '__prev'),
+            nextEl: $('.' + strSliderResult + '__next'),
+            disabledClass: strSliderResult + '__str-disabled',
         },
     });
 
+    /* Раскрытие меню */
     $('.header-main__hamburger').click(function(){
 
         $('.nav-top').slideToggle();
